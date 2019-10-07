330507f173ac1a164200daad94789a8c054bfeeb
Josh F <fowlerjosh93@gmail.com>
2019-10-07 16:20:45 -0400

    got prompt customer for item function to update table with new quantity after purchase

--- bamazon.js
+++ bamazon.js
@@ -29,76 +29,104 @@ function loadProducts() {
         console.table(res);
 
         //then prompt the customer for choice of item
-        promptCustomerForItem(res);
+        promptCustomerForItem();
 
     })
 }
 
 //prompt the user for a product id
-function promptCustomerForItem(){
-    
-    inquirer
-    .prompt([{
-        type: "input",
-        message: "What is the ID of the item you are searching for?",
-        name: "id"
-    }])
-    .then(function (answer){
-        connection.query("SELECT * FROM products WHERE ?"
-        [{
-            id: answer.id
-        }], 
-        function (err, res) {
-            if (err) throw err;
-        console.log("Searching for: " + answer.id);
-        promptCustomerForQuantity();
-    })
-    })
-}
-
-//prompt customer for quantity
-function promptCustomerForQuantity(){
-    inquirer
-    .prompt([{
-        type: "input",
-        message: "How many would you like to purchase?",
-        name: "quantity"
-    }])
-    .then(function (answer){
-        if(stock_quantity < answer.quantity){
-            console.log("Insufficient quantity!");
-            loadProducts()
-        }
-        else{
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                {
-                    stock_quantity: answer.quantity
-                },
-                function (err, res) {
-                    if (err) throw err;
-                    console.log(res.affectedRows + "Thank You For Your Purchase!");
-                    loadProducts();
-                    
+function promptCustomerForItem() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([{
+                type: "input",
+                message: "What is the ID of the item you are searching for?",
+                name: "itemID"
+            },
+            {
+                type: "input",
+                message: "How many would you like to buy?",
+                name: "amount"
+            }])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].id === parseInt(answer.itemID)) {
+                        chosenItem = results[i];
+                    }
                 }
-                ])
-        }
-    })
+                if (chosenItem.stock_quantity < parseInt(answer.amount)) {
+                    console.log("Insufficient quantity!")
+                    loadProducts();
+                }
+                else {
+                    var newQuantity = chosenItem.stock_quantity - parseInt(answer.amount);
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newQuantity
+                            },
+                            {
+                                id: answer.itemID
+                            }
 
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Thank you for your purchase!");
+                            loadProducts();
+                        }
+                    );
+                }
+            })
+    })
 }
+
+// //prompt customer for quantity
+// function promptCustomerForQuantity(){
+//     inquirer
+//     .prompt([{
+//         type: "input",
+//         message: "How many would you like to purchase?",
+//         name: "quantity"
+//     }])
+//     .then(function (answer){
+//         if(stock_quantity < answer.quantity){
+//             console.log("Insufficient quantity!");
+//             loadProducts()
+//         }
+//         else{
+//             connection.query(
+//                 "UPDATE products SET ? WHERE ?",
+//                 [
+//                 {
+//                     stock_quantity: answer.quantity
+//                 },
+//                 function (err, res) {
+//                     if (err) throw err;
+//                     console.log(res.affectedRows + "Thank You For Your Purchase!");
+//                     loadProducts();
+
+//                 }
+//                 ])
+//         }
+//     })
+
+// }
 
 //purchase function to buy desired item
-function makePurchase(){
+// function makePurchase(){
 
-}
+// }
 
-//check the inventory to see if the user choice exists in inventory
-function checkInventory(){
+// //check the inventory to see if the user choice exists in inventory
+// function checkInventory(){
 
-}
+// }
 
-//check to see if user wants to exit or continue (optional)
-function checkIfShouldExit(){
+// //check to see if user wants to exit or continue (optional)
+// function checkIfShouldExit(){
 
-}
+// 
